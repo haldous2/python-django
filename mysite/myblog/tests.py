ca2ebac0bb05645f2b2d28c7bfedbd5f46378db2
eric.westman <haldous2@gmail.com>
2015-05-21 18:22:15 -0700

    first commit

---
+++ mysite/myblog/tests.py
@@ -0,0 +1,20 @@
+from django.test import TestCase
+from myblog.models import Post
+
+# Create your tests here.
+
+from django.test import TestCase
+from django.contrib.auth.models import User
+
+class PostTestCase(TestCase):
+    
+    fixtures = ['myblog_test_fixture.json', ]
+
+    def setUp(self):
+        self.user = User.objects.get(pk=1)
+
+    def test_unicode(self):
+        expected = "This is a title"
+        p1 = Post(title=expected)
+        actual = unicode(p1)
+        self.assertEqual(expected, actual)
